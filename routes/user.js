0bb63a1979263619758717264f2db2343bb8b63c
Jack Ratner <jackratner@gmail.com>
2013-10-05 09:48:08 -0400

    all connections without sessions redirect to splash page. only people on email list can sign in.

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 var GoogleStrategy = require('passport-google').Strategy;
 var googlePaths = {};
 var userAuth = require('../auth/userAuth');
-var config = require('./config.json');
+var config = require('../config.json');
 
 if (config.development) {
 	googlePaths.returnURL = 'http://localhost:3000/auth/google/return';
@@ -15,36 +15,42 @@ if (config.development) {
 
 module.exports = function(app) {
 	var User = app.db.User;
-passport.use(new GoogleStrategy(googlePaths, function(identifier, profile, done) {
-    User.findOrCreateByGoogleEmail(profile, function(err, user) {
-        done(err, user);
-    });
-}));
+	passport.use(new GoogleStrategy(googlePaths, function(identifier, profile, done) {
+		User.findOrCreateByGoogleEmail(profile, function(err, user) {
+			if (err) {
+				return done(null, false, { message: 'Not allowed access'});
+			}
+			if (!user) {
+				return done(null, false, { message: 'Something went wrong'});
+			}
+			done(null, user);
+		});
+	}));
 
-passport.serializeUser(function(user, done) {
-    done(null, user._id);
-});
+	passport.serializeUser(function(user, done) {
+		done(null, user._id);
+	});
 
-passport.deserializeUser(function(_id, done) {
-    User.findOne({_id: _id}, function(err, user) {
-        done(err, user);
-    });
-});
+	passport.deserializeUser(function(_id, done) {
+		User.findOne({_id: _id}, function(err, user) {
+			done(err, user);
+		});
+	});
 
 
-/*
- * Routes!
- */
+	/*
+	 * Routes!
+	 */
 
-app.get('/auth/google', passport.authenticate('google'));
+	app.get('/auth/google', passport.authenticate('google'));
 
-app.get('/auth/google/return', passport.authenticate('google', { 
-    successRedirect: '/',
-    failureRedirect: '/join'
-}));
+	app.get('/auth/google/return', passport.authenticate('google', { 
+		successRedirect: '/home',
+		failureRedirect: '/splash'
+	}));
 
-app.get('/signOut', userAuth.signedIn, function(req, res) {
-	req.session.destroy();
-	res.redirect('/');
-});
+	app.get('/signOut', userAuth.signedIn, function(req, res) {
+		req.session.destroy();
+		res.redirect('/splash');
+	});
 };
