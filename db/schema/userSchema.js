4e7ccdf42ced094e6f035d6b4fa3b98f372803bc
jackrr <jackratner@gmail.com>
2013-12-03 12:58:42 -0500

    does this work?

--- db/schema/userSchema.js
+++ db/schema/userSchema.js
@@ -9,8 +9,8 @@ var userSchema = new Schema({
 		pseudo: String
 	},
 	// email: {type: String, required: true, enum: permittedUsers.users}
-	// email: {type: String, required: true}
-	email: {type: String, required: true, match: /^[A-Z0-9._%+-]+@oberlin.edu/}
+	email: {type: String, required: true}
+	// email: {type: String, required: true, match: /^[A-Z0-9._%+-]+@oberlin.edu/}
 });
 
 userSchema.pre('save', function(next) {
